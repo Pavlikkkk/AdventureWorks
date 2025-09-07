171d643b9da4838e3d387af42cce0591e34b6a1e
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-07 13:07:00 +0400

    Create Album.sql

---
+++ Chinook/ddl/Album.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Album`
+(
+    `AlbumId` INT NOT NULL AUTO_INCREMENT,
+    `Title` NVARCHAR(160) NOT NULL,
+    `ArtistId` INT NOT NULL,
+    CONSTRAINT `PK_Album` PRIMARY KEY  (`AlbumId`)
+);
+
+ALTER TABLE `Album` ADD CONSTRAINT `FK_AlbumArtistId`
+    FOREIGN KEY (`ArtistId`) REFERENCES `Artist` (`ArtistId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+CREATE INDEX `IFK_AlbumArtistId` ON `Album` (`ArtistId`);
