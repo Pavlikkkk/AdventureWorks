553de2634a5e0a3dc5a0d0923fd582ade7b103ce
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-07 16:57:10 +0400

    Update chroma_loader_full.py

--- Scripts/chroma_loader_full.py
+++ Scripts/chroma_loader_full.py
@@ -1,6 +1,11 @@
 import os
 import chromadb
-import git_parser as gp  # adjust if your module name differs
+import git_parser as gp
+import tiktoken
+
+
+def num_tokens(text, enc):
+    return len(enc.encode(text))
 
 
 class ChromaFullLoader:
@@ -9,8 +14,7 @@ class ChromaFullLoader:
 
 
     def create_collection(self,collection_name):
-        """ Drops and recreates a new collection """
-        # BASE_PATH = "/Volumes/D/AI/test_chromadb"
+        """Drops and recreate a new collection"""
         os.makedirs(self.base_path, exist_ok=True)
 
         # Use a persistent client that saves to disk
@@ -25,15 +29,17 @@ class ChromaFullLoader:
 
 
     def load_documents(self, collection):
-        """ Runs a full load into a collection. Uses table name as id """
+        """Runs a full load into a collection. Uses table name as id"""
+        enc = tiktoken.encoding_for_model("gpt-4o-mini") # for token calculation
         ids, documents, metadatas = [], [], []
         for table_name, table_data in tables.items():
             sha, size, ddl = table_data[0], table_data[1], table_data[2]
-            doc_id = table_name 
+            tokens = num_tokens(ddl, enc)
+            doc_id = table_name # crated id as table name, contains only last changes
 
             ids.append(doc_id)
             documents.append(ddl)
-            metadatas.append({ "table_name": table_name,"sha": sha,"size": size})
+            metadatas.append({ "table_name": table_name,"sha": sha,"size": size, "tokens": tokens })
 
         collection.upsert(ids=ids, documents=documents, metadatas=metadatas)
 
@@ -45,8 +51,8 @@ if __name__ == '__main__':
     )
     tables, errors = get_objects.get_git_objects(['ddl'])
     # Create and load collection
-    objects = ChromaFullLoader("yor_path")
-    collection = objects.create_collection("yor_collection_name")
+    objects = ChromaFullLoader("your_path")
+    collection = objects.create_collection("your_collection_name")
     objects.load_documents(collection)
     # Sanity check
     print("Collection:", collection.name)
