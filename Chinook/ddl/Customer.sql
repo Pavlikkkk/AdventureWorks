d992abf100de69368307712b3f4142c562a2e33f
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-07 13:10:03 +0400

    Create Customer.sql

---
+++ Chinook/ddl/Customer.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `Customer`
+(
+    `CustomerId` INT NOT NULL AUTO_INCREMENT,
+    `FirstName` NVARCHAR(40) NOT NULL,
+    `LastName` NVARCHAR(20) NOT NULL,
+    `Company` NVARCHAR(80),
+    `Address` NVARCHAR(70),
+    `City` NVARCHAR(40),
+    `State` NVARCHAR(40),
+    `Country` NVARCHAR(40),
+    `PostalCode` NVARCHAR(10),
+    `Phone` NVARCHAR(24),
+    `Fax` NVARCHAR(24),
+    `Email` NVARCHAR(60) NOT NULL,
+    `SupportRepId` INT,
+    CONSTRAINT `PK_Customer` PRIMARY KEY  (`CustomerId`)
+);
+ALTER TABLE `Customer` ADD CONSTRAINT `FK_CustomerSupportRepId`
+    FOREIGN KEY (`SupportRepId`) REFERENCES `Employee` (`EmployeeId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+CREATE INDEX `IFK_CustomerSupportRepId` ON `Customer` (`SupportRepId`);
