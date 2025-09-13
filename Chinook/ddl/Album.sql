47b310eb81eb6f0b2afb44e84da81d5579fc6f16
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-13 22:27:06 +0400

    Update Album.sql

--- Chinook/ddl/Album.sql
+++ Chinook/ddl/Album.sql
@@ -10,3 +10,5 @@ ALTER TABLE `Album` ADD CONSTRAINT `FK_AlbumArtistId`
     FOREIGN KEY (`ArtistId`) REFERENCES `Artist` (`ArtistId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 CREATE INDEX `IFK_AlbumArtistId` ON `Album` (`ArtistId`);
+
+-- description: The table Album contains info about artists and their titles. Other tables reference that table based on ArtistId column
