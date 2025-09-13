164529e8ba1775dd7bff3b94075731c4ddf3f8de
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-13 22:30:19 +0400

    Update Artist.sql

--- Chinook/ddl/Artist.sql
+++ Chinook/ddl/Artist.sql
@@ -4,3 +4,5 @@ CREATE TABLE `Artist`
     `Name` NVARCHAR(120),
     CONSTRAINT `PK_Artist` PRIMARY KEY  (`ArtistId`)
 );
+
+-- description: The table Artist contains artists' names. The other tables might reference this table 
