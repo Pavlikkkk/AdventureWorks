d5973f3b6c6b82d495e0cf4710b93b7c03e5c2f3
Pavel <42521657+Pavlikkkk@users.noreply.github.com>
2025-09-20 21:39:34 +0400

    Update Customer.sql

--- Chinook/ddl/Customer.sql
+++ Chinook/ddl/Customer.sql
@@ -19,3 +19,5 @@ ALTER TABLE `Customer` ADD CONSTRAINT `FK_CustomerSupportRepId`
     FOREIGN KEY (`SupportRepId`) REFERENCES `Employee` (`EmployeeId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 CREATE INDEX `IFK_CustomerSupportRepId` ON `Customer` (`SupportRepId`);
+
+-- -- description: The table Customer contains info about customers. 
